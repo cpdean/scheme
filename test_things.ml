afb780519d9ce9b9a8d1030e2b0bba26a746e76a
Conrad Dean <cdean@squarespace.com>
2017-01-16 23:05:53 -0500

    fix stray ref

--- test_things.ml
+++ test_things.ml
@@ -12,11 +12,6 @@ open OUnit2;;
   | `Null       -> output_string outc "null"
 *)
 
-let assert_parsed code_str parsed_ast = function test_ctxt ->
-        assert_equal
-            (Test.parse_with_error (Lexing.from_string code_str))
-            parsed_ast
-
 let k_assert_parsed code_str parsed_ast = function test_ctxt ->
         assert_equal
             (Ktest.parse_with_error (Lexing.from_string code_str))
@@ -26,44 +21,41 @@ let k_assert_parsed code_str parsed_ast = function test_ctxt ->
 let suite =
     "suite">:::
     [
-        "test4">:: assert_parsed
-             "{\"a\": 2}"
-            (Some (`Assoc [("a", `Int 2)]))
-        ;
-        "test5">:: assert_parsed
-            "{
-                \"a\": 
-                2
-            }"
-            (Some (`Assoc [("a", `Int 2)]))
-        ;
         "empty">:: k_assert_parsed
             "()"
             (Some (`Expression []))
         ;
+
         "empty with one space">:: k_assert_parsed
             "( )"
             (Some (`Expression []))
         ;
+
         "single">:: k_assert_parsed
             "(a)"
             (Some (`Expression [`Avalue]))
         ;
+
         "single with space to right">:: k_assert_parsed
             "(a )"
             (Some (`Expression [`Avalue]))
         ;
+
         "single with space to left">:: k_assert_parsed
             "( a)"
             (Some (`Expression [`Avalue]))
         ;
+
         "two">:: k_assert_parsed
             "(a a)"
             (Some (`Expression [`Avalue; `Avalue]))
+
         ;
+
         "three">:: k_assert_parsed
             "(a a a)"
             (Some (`Expression [`Avalue; `Avalue; `Avalue]))
+
         ;
 ]
 ;;
